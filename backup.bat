a03a245fd7f3a4145fa0bd91442f82f56864b8fd
Игорь Десятов <DesyatovIV@gkosar.corp>
2020-11-06 16:30:56 +0400

    First commit. Repo for backups through bats

---
+++ backup.bat
@@ -0,0 +1,39 @@
+REM СОЗДАНИЕ РЕЗЕРВНОЙ КОПИИ БАЗЫ ДАННЫХ POSTGRESQL
+CLS
+ECHO OFF
+CHCP 1251
+REM Установка переменных окружения
+SET PGBIN=c:\Program Files\PostgreSQL\11\bin
+SET PGDATABASE=webGbu
+SET PGHOST=localhost
+SET PGPORT=5433
+SET PGUSER=postgres
+SET PGPASSWORD=root
+REM Смена диска и переход в папку из которой запущен bat-файл
+%~d0
+CD %~dp0
+REM Формирование имени файла резервной копии и имени файла отчёта
+SET DATETIME=%DATE:~6,4%-%DATE:~3,2%-%DATE:~0,2% %TIME:~0,2%-%TIME:~3,2%-%TIME:~6,2%
+SET DUMPFILE=%PGDATABASE% %DATETIME%.backup
+SET LOGFILE=%PGDATABASE% %DATETIME%.log
+SET DUMPPATH="Backup\%DUMPFILE%"
+SET LOGPATH="Backup\%LOGFILE%"
+REM Создание папки для бекапов, если она не существует
+IF NOT EXIST Backup MD Backup
+IF NOT EXIST %DUMPFILE% MD %DUMPFILE%
+IF NOT EXIST %LOGFILE% MD %LOGFILE%
+REM Создание резервной копии
+CALL "%PGBIN%\pg_dump.exe" --no-password --format=custom --verbose --file=%DUMPPATH% 2>%LOGPATH%
+REM Анализ кода завершения
+IF NOT %ERRORLEVEL%==0 GOTO Error
+GOTO Successfull
+REM В случае ошибки удаляется поврежденная резервная копия и делается соответствующая запись в журнале
+:Error
+MSG * "Ошибка при создании резервной копии базы данных. Смотрите backup.log."
+ECHO %DATETIME% Ошибки при создании резервной копии базы данных %DUMPFILE%. Смотрите отчет %LOGFILE%. >> backup.log
+GOTO End
+REM В случае удачного резервного копирования просто делается запись в журнал
+:Successfull
+ECHO %DATETIME% Успешное создание резервной копии %DUMPFILE% >> backup.log
+GOTO End
+:End
